3f4e5a611714fa01b34643b430da5b7fc71710d6
Maksim <epic.twod@mail.ru>
2023-01-24 22:26:15 +0300

    Task 2

--- HW1/Task2/src/defs.java
+++ HW1/Task2/src/defs.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class defs {
+public class defs {
+    static boolean isPrime(int a)
+    {
+        for(int i = 2; i < a/2; i++)
+        {
+            if (a%i==0) return false;
+        }
+        return true;
+    }
 }
